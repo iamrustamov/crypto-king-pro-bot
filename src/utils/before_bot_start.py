445b408ddbaeca3fea6d303b0256d4980a07fcca
Rustamov Daler <iamrustamov@gmail.com>
2024-07-21 19:15:19 +0300

    commit price_list_handler.py

--- src/utils/before_bot_start.py
+++ src/utils/before_bot_start.py
@@ -1,7 +1,6 @@
 from aiogram import Bot
 from aiogram.types import BotName, Message
 
-from utils.bot_commands import bot_commands
 from config import settings
 
 
@@ -15,7 +14,6 @@ async def start_bot(bot: Bot) -> Message:
     :return: Message
     """
     # Get commands
-    await bot_commands(bot)
     bot_name: BotName = await bot.get_my_name()
     return await bot.send_message(
         chat_id=settings.ADMINS_ID[0], text=f'The "{bot_name.name}" bot has been launched!'
